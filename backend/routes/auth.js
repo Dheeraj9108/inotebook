c08402c92c378289d1cd19ba32129d3ff3698553
Dheeraj <poojarydheeraj665@gmail.com>
2022-05-10 22:46:29 +0530

    vid 47

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const User = require('../models/User');
 const {body,validationResult} = require('express-validator');
 //Creat a user using : POST "/api/auth"
-router.post('/',[
+router.post('/CreateUser',[
     body('name','Enter a valid email').isLength({min:3}),
     body('email').isEmail(),
     body('password').isLength({min:5})
-],(req,res)=>{
+],async(req,res)=>{
     // obj = {
     //     a:"this",
     //     number: 34
@@ -20,12 +20,27 @@ router.post('/',[
         res.status(400).json({errors:errors.array()});
     }
     // res.send(req.body);
-    User.create({
+    //check weather user with this email exit already
+    try {
+        
+    
+    let user = await User.findOne({email : req.body.email});
+    if(user){
+        return res.status(400).json({error : "Sorry User with this email Already exits"})
+    }
+    let users = await User.create({
         name:req.body.name,
         email:req.body.email,
         password:req.body.password,
-    }).then(user => res.json(user)).catch(err => {console.log(err)
-    res.json({error:'Please enter a valid email',msg:err.msg})});
+    })
+    res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error Occured") ;   
+    }
+    
+    // .then(user => res.json(user)).catch(err => {console.log(err)
+    // res.json({error:'Please enter a valid email',msg:err.msg})});
     // console.log(req.body);
     // res.send("hellow")
 })
