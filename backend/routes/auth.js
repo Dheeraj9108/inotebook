349cbb0ee708f198dd4055a483e76b674c906603
Dheeraj <poojarydheeraj665@gmail.com>
2022-05-19 20:08:20 +0530

    vid 49

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const {body,validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "PooJaryd333heerajK91uma0816@107$7r";
+
 //Creat a user using : POST "/api/auth"
 router.post('/CreateUser',[
     body('name','Enter a valid email').isLength({min:3}),
@@ -28,12 +33,23 @@ router.post('/CreateUser',[
     if(user){
         return res.status(400).json({error : "Sorry User with this email Already exits"})
     }
-    let users = await User.create({
+
+    const salt = await bcrypt.genSalt(10);
+    secpass = await bcrypt.hash(req.body.password,salt);
+    users = await User.create({
         name:req.body.name,
         email:req.body.email,
-        password:req.body.password,
+        password:secpass
     })
-    res.json(user);
+    const data = {
+        user:{
+            id:users.id
+        }
+    } 
+    const authtoken = jwt.sign(data,JWT_SECRET);
+    // console.log(data);
+    // res.json(users);
+    res.json({authtoken});
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Some error Occured") ;   
