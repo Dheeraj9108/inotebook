a98701913b58a774588ad43ff5107b2be18747e2
Dheeraj <poojarydheeraj665@gmail.com>
2022-05-21 18:14:48 +0530

    vid 52

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
-
+const {Schema} = mongoose;
 const NotesSchema = new Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'user'
+    },
     title:{
         type: String,
         required:true
     },
     description:{
-            type:String,
-            required: true,
+        type:String,
+        required: true,
     },
     tag:{
         type:String,
@@ -18,4 +22,4 @@ const NotesSchema = new Schema({
         default:Date.now
     }
 });
-module.exports = mongoose.model(notes,NotesSchema);
+module.exports = mongoose.model('notes',NotesSchema);
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,61 @@
 const express = require('express');
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const Note = require('../models/Note');
+const { body, validationResult } = require('express-validator');
 
-router.get('/',(req,res)=>{
-  
-    res.json([]);
+//rout:1get all the notes
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Notes.find({ user: req.user.id });
+        res.json([notes]);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error Occured");
+    }
+
+})
+//route 2
+router.post('/addnote', fetchuser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Enter a valid description').isLength({ min: 5 })
+], async (req, res) => {
+    try {
+        const { title, description, tag } = req.body;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({ errors: errors.array() });
+        }
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        })
+        const savednote = await note.save();
+        res.json(savednote);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error Occured");
+    }
+})
+
+
+//route 3:update an existing note using POST "/api/auth/updatenote".login required
+
+
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const{title,description,tag} = req.body;
+    const newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+
+    // find the note to be updated
+    let note = await Note.findById(req.params.id)
+    if(!note){
+        return res.status(404).send("Not found");
+    }
+    if(note.user.toString() !== req.user.id){return res.status(401).send("Not allowed")}
+
+    note = await Note.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true});
+    res.json(note);
 })
 module.exports = router
