3caaa21677111f47c10ce4d962df3a0bba1b2eca
Dheeraj <poojarydheeraj665@gmail.com>
2022-05-24 23:38:52 +0530

    vid 65

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const connectToMongo = require('./db');
 const express = require('express'); 
+var cors = require('cors'); 
 
 connectToMongo();
 const app = express();
+app.use(cors());
 const port = 5000;
 
 app.use(express.json());
