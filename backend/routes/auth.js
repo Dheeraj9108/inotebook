7a04a75a904fe1d3e95c4afd38a43e9c887e9e31
Dheeraj <poojarydheeraj665@gmail.com>
2022-05-26 22:22:00 +0530

    vid 70

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -14,6 +14,7 @@ router.post('/CreateUser',[
     body('email').isEmail(),
     body('password').isLength({min:5})
 ],async(req,res)=>{
+    let sucess = false;
     // obj = {
     //     a:"this",
     //     number: 34
@@ -23,7 +24,7 @@ router.post('/CreateUser',[
     // user.save();
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        res.status(400).json({errors:errors.array()});
+        res.status(400).json({sucess,errors:errors.array()});
     }
     // res.send(req.body);
     //check weather user with this email exit already
@@ -31,25 +32,26 @@ router.post('/CreateUser',[
     
     let user = await User.findOne({email : req.body.email});
     if(user){
-        return res.status(400).json({error : "Sorry User with this email Already exits"})
+        return res.status(400).json({sucess,error : "Sorry User with this email Already exits"})
     }
 
     const salt = await bcrypt.genSalt(10);
     secpass = await bcrypt.hash(req.body.password,salt);
-    users = await User.create({
+    user = await User.create({
         name:req.body.name,
         email:req.body.email,
         password:secpass
     })
     const data = {
         user:{
-            id:users.id
+            id:user.id
         }
     } 
     const authtoken = jwt.sign(data,JWT_SECRET);
     // console.log(data);
     // res.json(users);
-    res.json({authtoken});
+    sucess = true;
+    res.json({sucess,authtoken});
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Some error Occured") ;   
